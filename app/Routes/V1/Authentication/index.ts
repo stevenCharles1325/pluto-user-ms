b0ec6049313d8b61203836d697e9326c51fbde13
stevenCharles1325 <stevencharles1325@gmail.com>
2023-08-06 13:01:01 +0800

    chore: update prefix
    Signed-off-by: stevenCharles1325 <stevencharles1325@gmail.com>

--- app/Routes/V1/Authentication/index.ts
+++ app/Routes/V1/Authentication/index.ts
@@ -5,6 +5,7 @@ Route.group(() => {
   Route.post('/login', 'AuthenticationController.login')
   Route.post('/register', 'AuthenticationController.register')
   Route.delete('/logout', 'AuthenticationController.logout')
+  Route.put('/refresh-token', 'AuthenticationController.refresh')
 })
   .prefix('/auth')
   .namespace('App/Domains/Authentication/Controllers')
--- start/routes.ts
+++ start/routes.ts
@@ -3,5 +3,7 @@ import { requireAll } from '@ioc:Adonis/Core/Helpers'
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  requireAll(join(__dirname, '../app/Routes/V1'))
-}).prefix('v1')
+  Route.group(() => {
+    requireAll(join(__dirname, '../app/Routes/V1'))
+  }).prefix('v1')
+}).prefix('api')
